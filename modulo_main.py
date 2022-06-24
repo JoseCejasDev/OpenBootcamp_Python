364cac8bad596ee9e06b00d96e269ad93439b961
JoseCejasDev <88114719+JoseCejasDev@users.noreply.github.com>
2022-06-24 14:51:04 -0300

    modulo calculadora
    En este ejercicio tendréis que crear un módulo que contenga las operaciones básicas de una calculadora: sumar, restar, multiplicar y dividir.
    Este módulo lo importaréis a un archivo python y llamaréis a las funciones creadas. Los resultados tenéis que mostrarlos por consola.

---
+++ modulo_main.py
@@ -0,0 +1,6 @@
+import modulo_operaciones
+
+print(modulo_operaciones.suma(10, 5))
+print(modulo_operaciones.resta(10, 5))
+print(modulo_operaciones.multiplicacion(10, 5))
+print(modulo_operaciones.division(10, 5))
